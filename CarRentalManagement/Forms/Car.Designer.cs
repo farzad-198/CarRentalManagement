c9ea01d7f513781a03acaf4457e1beb00e0f75bd
farzad-198 <farzadjafarzadehtey@gmail.com>
2026-03-31 23:45:26 +0200

    fixa car.cs datagridview

--- CarRentalManagement/Forms/Car.Designer.cs
+++ CarRentalManagement/Forms/Car.Designer.cs
@@ -350,6 +350,7 @@
             this.dataGridView1.RowTemplate.Height = 28;
             this.dataGridView1.Size = new System.Drawing.Size(733, 504);
             this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             // 
             // carIDDataGridViewTextBoxColumn
             // 
